e51eee032918c67b2cd9178982710062f56dbd6b
xwp <xwp_fullstack@163.com>
2019-06-25 09:54:57 +0800

    udpate

--- bookpro/urls.py
+++ bookpro/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include('booktest.urls'))
+    url(r'^',include('booktest.urls')),
+    url(r'^docs/', include_docs_urls(title='API')),
 
 
 ]
